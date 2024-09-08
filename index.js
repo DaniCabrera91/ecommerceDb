fd99b93e215846b76927e071d1e4754855d40f86
denisc911 <denis.cabrera1990@gmail.com>
2024-09-08 19:07:14 +0200

    Agregado en el index el cloudinary, ademas se instala multer y cloudinary

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ const cors = require('cors')
 const app = express()
 const PORT = 3000;
 const { typeError } = require('./middlewares/errors')
+const cloudinary = require('cloudinary').v2
+const { CloudinaryStorage } = require('multer-storage-cloudinary')
+const multer = require('multer')
+
+cloudinary.config({
+    cloud_name: 'dhn4hwunj',
+    api_key: '879184289568791',
+    api_secret: '0C95qm-asi74Ebrie7KjpHW12as',
+    })
+    
+    const storage = new CloudinaryStorage({
+        cloudinary,
+        params: {
+        folder: 'ecommerce_img',
+        },
+        })
+
+        const upload = multer({ storage })
+
 
 app.use(cors())
 
@@ -18,3 +37,11 @@ app.use('/reviews', require('./routes/reviews'))
 app.use(typeError)
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`))
+
+app.post('/upload', upload.single('picture'), (req, res) => {
+    res.status(200).json({
+    url: req.file,
+    })
+    })
+    
+    
---
+++ routes/orders copy.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+const OrderController = require('../controllers/OrderController')
+const { authentication } = require('../middlewares/authentication')
+
+router.post('/', authentication, OrderController.create)
+router.get('/', OrderController.getAll)
+
+module.exports = router
+
+
